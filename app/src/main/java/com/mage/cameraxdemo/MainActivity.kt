28b990c38a153df0a3ae5d87579bb6249e5d64fb
mage <1275054668@qq.com>
2021-05-31 10:46:05 +0800

    分析图片功能

--- app/src/main/java/com/mage/cameraxdemo/MainActivity.kt
+++ app/src/main/java/com/mage/cameraxdemo/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.mage.cameraxdemo
 
 import android.Manifest
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.util.Size
+import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
@@ -11,6 +16,8 @@ import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
 import com.google.common.util.concurrent.ListenableFuture
 import kotlinx.android.synthetic.main.activity_main.*
+import java.nio.ByteBuffer
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
     private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
@@ -30,16 +37,33 @@ class MainActivity : AppCompatActivity() {
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         //默认你会同意权限，不同意就是自己的事了
+
+        val imageAnalysis = ImageAnalysis.Builder()
+            .setTargetResolution(Size(1280, 720))
+            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+            .build()
+        var executor = Executors.newFixedThreadPool(5)
+        imageAnalysis.setAnalyzer(executor, ImageAnalysis.Analyzer { image ->
+            val rotationDegrees = image.imageInfo.rotationDegrees
+            iv.setImageBitmap(imageProxyToBitmap(image))
+            image.close()
+        })
         cameraProviderFuture.addListener(Runnable {
             val cameraProvider = cameraProviderFuture.get()
-            bindPreview(cameraProvider)
+            bindPreview(cameraProvider,imageAnalysis)
         }, ContextCompat.getMainExecutor(this))
-    }
 
+    }
+    private fun imageProxyToBitmap(image: ImageProxy): Bitmap {
+        val buffer: ByteBuffer = image.planes[0].buffer
+        val bytes = ByteArray(buffer.remaining())
+        buffer.get(bytes)
+        return BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+    }
     /**
      * 绑定预览view
      */
-    fun bindPreview(cameraProvider: ProcessCameraProvider) {
+    fun bindPreview(cameraProvider: ProcessCameraProvider, imageAnalysis: ImageAnalysis) {
         var preview: Preview = Preview.Builder()
             .build()
 
@@ -49,6 +73,6 @@ class MainActivity : AppCompatActivity() {
 
         preview.setSurfaceProvider(previewView.getSurfaceProvider())
 
-        var camera = cameraProvider.bindToLifecycle(this as LifecycleOwner, cameraSelector, preview)
+        var camera = cameraProvider.bindToLifecycle(this as LifecycleOwner, cameraSelector,imageAnalysis, preview)
     }
 }
